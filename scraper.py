31b64017ea29e231728422dc8105845dd0354631
Harsha-10 <dharsha9949@gmail.com>
2025-01-07 11:46:13 +0530

    modified code 8

--- scraper.py
+++ scraper.py
@@ -30,8 +30,7 @@ class TwitterScraper:
 
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        CHROMEDRIVER_PATH = './chromedriver.exe'
-        service = Service(executable_path=CHROMEDRIVER_PATH)
+        service = Service(executable_path=ChromeDriverManager.install())
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
     def get_ip_address(self):
